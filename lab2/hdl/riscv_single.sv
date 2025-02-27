5f2eb4ff85fd416ef92444567ab9307d39e1595a
Frosty-Animal <92558977+Frosty-Animal@users.noreply.github.com>
2025-02-27 17:30:08 -0600

    Update riscv_single.sv

--- lab2/hdl/riscv_single.sv
+++ lab2/hdl/riscv_single.sv
@@ -80,8 +80,8 @@ module riscvsingle (input  logic        clk, reset,
 		    input  logic [31:0] ReadData);
    
    logic 				ALUSrc, RegWrite, Jump, Zero;
-   logic [1:0] 				ResultSrc, ImmSrc;
-   logic [2:0] 				ALUControl;
+   logic [1:0] 				 ImmSrc;
+   logic [2:0] 				ResultSrc, ALUControl;
    
    controller c (Instr[6:0], Instr[14:12], Instr[30], Zero,
 		 ResultSrc, MemWrite, PCSrc,
@@ -99,7 +99,7 @@ module controller (input  logic [6:0] op,
 		   input  logic [2:0] funct3,
 		   input  logic       funct7b5,
 		   input  logic       Zero,
-		   output logic [1:0] ResultSrc,
+		   output logic [2:0] ResultSrc,
 		   output logic       MemWrite,
 		   output logic       PCSrc, ALUSrc,
 		   output logic       RegWrite, Jump,
@@ -117,10 +117,10 @@ module controller (input  logic [6:0] op,
 endmodule // controller
 
 module maindec (input  logic [6:0] op,
-		output logic [1:0] ResultSrc,
-		output logic 	   MemWrite,
-		output logic 	   Branch, ALUSrc,
-		output logic 	   RegWrite, Jump,
+		output logic [2:0] ResultSrc,
+		output logic 	     MemWrite,
+		output logic 	     Branch, ALUSrc,
+		output logic 	     RegWrite, Jump,
 		output logic [1:0] ImmSrc,
 		output logic [1:0] ALUOp);
    
@@ -132,13 +132,15 @@ module maindec (input  logic [6:0] op,
    always_comb
      case(op)
        // RegWrite_ImmSrc_ALUSrc_MemWrite_ResultSrc_Branch_ALUOp_Jump
-       7'b0000011: controls = 11'b1_00_1_0_01_0_00_0; // lw
-       7'b0100011: controls = 11'b0_01_1_1_00_0_00_0; // sw
-       7'b0110011: controls = 11'b1_xx_0_0_00_0_10_0; // R–type
-       7'b1100011: controls = 11'b0_10_0_0_00_1_01_0; // beq
-       7'b0010011: controls = 11'b1_00_1_0_00_0_10_0; // I–type ALU
-       7'b1101111: controls = 11'b1_11_0_0_10_0_00_1; // jal
-       default: controls = 11'bx_xx_x_x_xx_x_xx_x; // ???
+       7'b0000011: controls = 12'b1_00_1_0_001_0_00_0; // lw
+       7'b0100011: controls = 12'b0_01_1_1_000_0_00_0; // sw
+       7'b0110011: controls = 12'b1_xx_0_0_000_0_10_0; // R–type
+       7'b1100011: controls = 12'b0_10_0_0_000_1_01_0; // beq
+       7'b0010011: controls = 12'b1_00_1_0_000_0_10_0; // I–type ALU 
+       7'b1101111: controls = 12'b1_11_0_0_010_0_00_1; // jal
+       7'b0010111: controls = 12'b1_11_1_0_100_1_00_0; // auipc Ask about controls
+       7'b0110111: controls = 12'b1_11_1_0_100_0_00_0; // lui Ask about controls
+       default: controls = 12'bx_xx_x_x_xxx_x_xx_x; // ???
      endcase // case (op)
    
 endmodule // maindec
@@ -147,31 +149,35 @@ module aludec (input  logic       opb5,
 	       input  logic [2:0] funct3,
 	       input  logic 	  funct7b5,
 	       input  logic [1:0] ALUOp,
-	       output logic [2:0] ALUControl);
+	       output logic [3:0] ALUControl);
    
    logic 			  RtypeSub;
    
    assign RtypeSub = funct7b5 & opb5; // TRUE for R–type subtract
    always_comb
      case(ALUOp)
-       2'b00: ALUControl = 3'b000; // addition
-       2'b01: ALUControl = 3'b001; // subtraction
+       2'b00: ALUControl = 4'b0000; // addition
+       2'b01: ALUControl = 4'b0001; // subtraction
        default: case(funct3) // R–type or I–type ALU
 		  3'b000: if (RtypeSub)
-		    ALUControl = 3'b001; // sub
+		    ALUControl = 4'b0001; // sub
 		  else
-		    ALUControl = 3'b000; // add, addi
-		  3'b010: ALUControl = 3'b101; // slt, slti
-		  3'b110: ALUControl = 3'b011; // or, ori
-		  3'b111: ALUControl = 3'b010; // and, andi
-		  default: ALUControl = 3'bxxx; // ???
+		    ALUControl = 4'b0000; // add, addi
+		  3'b010: ALUControl = 4'b0101; // slt, slti
+		  3'b110: ALUControl = 4'b0011; // or, ori
+		  3'b111: ALUControl = 4'b0010; // and, andi
+      3'b011: ALUControl = 4'b0110; // sltu, sltiu
+      3'b100: ALUControl = 4'b0111; // Xor Xoxi
+      3'b001: ALUControl = 4'b1000; // sll slli
+      3'b101: ALUControl = 4'b1001; // srl srli
+		  default: ALUControl = 4'bxxx; // ??? 
 		endcase // case (funct3)       
      endcase // case (ALUOp)
    
 endmodule // aludec
 
 module datapath (input  logic        clk, reset,
-		 input  logic [1:0]  ResultSrc,
+		 input  logic [2:0]  ResultSrc,
 		 input  logic 	     PCSrc, ALUSrc,
 		 input  logic 	     RegWrite,
 		 input  logic [1:0]  ImmSrc,
@@ -217,13 +223,16 @@ module extend (input  logic [31:7] instr,
    always_comb
      case(immsrc)
        // I−type
-       2'b00:  immext = {{20{instr[31]}}, instr[31:20]};
+       3'b000:  immext = {{20{instr[31]}}, instr[31:20]};
        // S−type (stores)
-       2'b01:  immext = {{20{instr[31]}}, instr[31:25], instr[11:7]};
+       3'b001:  immext = {{20{instr[31]}}, instr[31:25], instr[11:7]};
        // B−type (branches)
-       2'b10:  immext = {{20{instr[31]}}, instr[7], instr[30:25], instr[11:8], 1'b0};       
+       3'b010:  immext = {{20{instr[31]}}, instr[7], instr[30:25], instr[11:8], 1'b0};       
        // J−type (jal)
-       2'b11:  immext = {{12{instr[31]}}, instr[19:12], instr[20], instr[30:21], 1'b0};
+       3'b011:  immext = {{12{instr[31]}}, instr[19:12], instr[20], instr[30:21], 1'b0};
+       // U-type
+       3'b100:  immext = {{12{instr[31]}}, instr[19:0]};
+
        default: immext = 32'bx; // undefined
      endcase // case (immsrc)
    
